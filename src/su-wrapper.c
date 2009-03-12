220255a2b2f2c7275a79241122af12d1e6187738
git <git@jupiter.metux.de>
2009-03-12 03:13:12 +0100

    Imported version 1.1.3.0

--- src/su-wrapper.c
+++ src/su-wrapper.c
@@ -3,374 +3,458 @@
     SU wrapper main file.
     
     copyrigh (c) enrico weigelt <weigelt@nibiru.thur.de>
+		 Werner Fink <werner@suse.de>
     
     this code is released under the terms of the 
     GNU Publics License.
 
 */
 
-/* TODO: use exec() insted of system -- 
-    this will overwrite the wrapper's process image and so save memory 
-*/
-
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include <fcntl.h>
 #include <pwd.h>
+#include <grp.h>
+#include <limits.h>
 #include <sys/types.h>
-#include "cmdline.h"
+extern char **environ;
 
 #define CONFIGFILE	"/etc/su-wrapper.conf"
 typedef const char * CHR;
+static int verbose = 0;
 
-int verbose = 0;
+static uid_t euid, ruid;
+static gid_t egid, rgid;
 
-CHR extract ( CHR fn )
+/*
+ * Internal logger, we may use vsyslog() instead vfprintf() here.
+ */
+static char *myname = "su-wrapper";
+static void _logger (const char *fmt, va_list ap)
 {
-	CHR key;
-
-	key = fn + strlen(fn);
-	if (key==fn) return NULL; /* hmm... no command */
-	
-	while ( (*key)!='/' ) {
-	    if (key==fn) return key;
-	    key--;
-	}
-	key++;
-	return key;
+	char buf[strlen(myname)+2+strlen(fmt)+1];
+	strcat(strcat(strcpy(buf, myname), ": "), fmt);
+	vfprintf(stderr, buf, ap);
+	return;
 }
 
-#define ALLOW	0
-#define DENY	1
+static inline const char* my_basename ( const char* name )
+{
+	if (name) return basename(name);
+	return "";
+}
 
-typedef struct tagENTRY ENTRY;
 
-struct tagENTRY {
-	/* this must match with the calling params */
-	char * called_user;
-	char * called_group;
-	char * called_command;
-	char * called_cmdline;
+/*
+ * Cry and exit.
+ */
+static void error (const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	_logger(fmt, ap);
+	va_end(ap);
+	exit (1);
+}
 
-	/* this will be executed */
-	char * run_user;
-	char * run_group;
-	char * run_command;
+/*
+ * Warn the user.
+ */
+static void warn (const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	_logger(fmt, ap);
+	va_end(ap);
+	return;
+}
 
-	char * params;
-		
-	ENTRY * next;
+struct entry_struct {
+        char * called_user;
+        char * called_group;
+        char * called_command;
+        char * called_cmdline;
+        char * run_user;
+        char * run_group;
+        char * run_command;
+        char * params[_POSIX_ARG_MAX + 1];
 };
+typedef struct entry_struct entry_t;
+static entry_t entry;
 
-ENTRY * entries = NULL; 
-
-int add_entry ( 
-    CHR called_user, CHR called_group, CHR called_command, CHR called_cmdline, 
-    CHR run_user, CHR run_group, CHR run_cmdline, CHR param
-)
+static int wildcard_match ( CHR mask, CHR value )
 {
-    ENTRY * ent;
-    ENTRY * walk;
+	int ret = 0;
 
-    ent = (ENTRY*)malloc(sizeof(ENTRY));
-    ent->next            = NULL;
-    ent->called_user     = strdup(called_user);
-    ent->called_group    = strdup(called_group);
-    ent->called_command  = strdup(called_command);
-    ent->called_cmdline  = strdup(called_cmdline);
-    ent->run_user        = strdup(run_user);
-    ent->run_group       = strdup(run_group);
-    ent->run_command     = strdup(run_cmdline);
-    ent->params          = strdup(param);
+	if (strlen(mask) == 1) {
+		if (*mask == '-') ret = 1;	/* ignore -> match any */
+		if (*mask == '*') ret = 1;	/* match any */
+	} else {				/* real wildcards not yet implemented, see regex(3) */
+		if (!strcmp(mask, value))	/* direct match */
+			ret = 1;
+	}
+	return ret;
+}
+
+static int cmdline_match (char * const mask, char * const argv[])
+{
+	int ret = 0;
     
-    /* we put it on the end of the list */    
-    if (entries==NULL) entries = ent;
-    else {
-	walk = entries;
-	while (walk->next) walk=walk->next;
-	walk->next = ent;
-    }
-}
+	if (!mask) { printf("FAULT!\n"); return 0; }
 
-int wildcard_match ( CHR mask, CHR value )
-{
-	if (strcmp(mask, "-")==0) return 1;	/* ignore -> match any */
-	if (strcmp(mask, "*")==0) return 1;	/* match any */
-	/* real wildcars not yet implemented */
-	if (strcmp(mask, value)==0) return 1;	/* direct match */
-	return 0;
-}
+	if (strlen(mask) == 1) {
+		if (*mask == '-') 		/* match none */
+			if (!*argv) ret = 1;
+		if (*mask == '*')   ret = 1;	/* match any */
+	} else {				/* real wildcards not yet implemented, see regex(3) */
+		char *l = strdupa(mask);
+		char *oslot;
+		int n, o[_POSIX_ARG_MAX];	/* o is our option vector for found options */
 
-int cmdline_match ( CHR mask, CHR value )
-{
-	if (wildcard_match(mask, value)) return 1;
-	else {
-		int mask_argc;
-		char * mask_argv[32];
-		int val_argc;
-		char * val_argv[32];
-		int x;
-		
-		mask_argc = SplitCommandLine ( mask, (char**)&mask_argv );
-		val_argc  = SplitCommandLine ( value,(char**) &val_argv );
-		
-		/* currently no wildcards are supported at this level */
-		if (mask_argc!=val_argc) goto __out__failed;
-		for (x=0; x<mask_argc; x++) {
-			if (strcmp(mask_argv[x], val_argv[x])) goto __out__failed;
-		}
-		
-		KillCommandLine ( mask_argc, (char**)&mask_argv );
-		KillCommandLine ( val_argc, (char**)&val_argv );
-		return 1;
-		
-__out__failed:
-		KillCommandLine ( mask_argc, (char**)&mask_argv );
-		KillCommandLine ( val_argc, (char**)&val_argv );
-		return 0;
-	}
-}
-			
-/* try to match this entry with our requirements */
-int try_to_match ( ENTRY * ent, CHR called_user, CHR called_group, 
-	CHR called_command, CHR called_cmdline )
-{
-	/* compare called user */
-	if (!wildcard_match(ent->called_user, called_user)) return 0;
-	/* compare the group */
-	if (!wildcard_match(ent->called_group, called_group)) return 0;
-	/* compare the called_command */		
-	if (!wildcard_match(ent->called_command, called_command)) return 0;
-	/* compare the called cmdline */
-	if (!cmdline_match(ent->called_cmdline, called_cmdline)) return 0;
-	return 1;
-}
-	
-ENTRY * FindMatching ( CHR called_user, CHR called_group, CHR called_cmd, CHR called_cmdline )
-{
-	ENTRY * walk;
-	ENTRY * found;
-	
-	if (called_user==NULL) called_user = "nobody";
-	if (called_group==NULL) called_group = "nogroup";
+		ret = 1;
+		if (!*argv)
+			goto out;
 
-	if (verbose) {
-	    printf("Trying to match : user=\"%s\" group=\"%s\" cmd=\"%s\" param=\"%s\"\n", 
-		called_user, called_group, called_cmd, called_cmdline );
-	}
-		    
-	found = NULL;
-	walk  = entries;
-	while (walk) {
-		if (try_to_match(walk, called_user, called_group, 
-			called_cmd, called_cmdline )) {
-			found = walk;
-			if (verbose) {
-			    printf("Found match: run_user=\"%s\" run_group=\"%s\" run_cmdline=\"%s\" \n",
-				found->run_user, found->run_group, found->run_command );
+		memset(o, 0, sizeof(o));
+		while ((oslot = strsep(&l, ",:")) && *oslot) {
+			for (n = 0; n < _POSIX_ARG_MAX && argv[n]; n++) {
+				if (!strcmp(oslot, argv[n]))
+					o[n]++;
 			}
 		}
-		walk = walk->next;
+
+						/* -lf is not -fl and not -l -f or -f -l */
+		ret = 1;
+		for (n = 0; n <= _POSIX_ARG_MAX && argv[n]; n++)
+			if (!o[n])
+				ret = 0;
 	}
-	
-	return found;
+out:
+	return ret;
+}
+			
+static inline char * get_current_user ()
+{
+	struct passwd * pw = getpwuid(getuid());
+	char * ret = "nobody";
+
+	if (pw)
+		ret = pw->pw_name;
+	else
+		warn("could not determine current user name ... defaulting to \"nobody\"\n");
+
+	return ret;
 }
 
-char * get_current_user ()
+static inline char * get_current_group ()
 {
-	struct passwd * pw;
-	int uid;
-	
-	uid = getuid();
-	setpwent();
-	
-	while (pw = getpwent()) {
-		if (pw->pw_uid == uid) {
-			return pw->pw_name;
+	struct group * gr = getgrgid(getgid());
+	char * ret = "nogroup";
+
+	if (gr)
+		ret = gr->gr_name;
+	else
+		warn("could not determine current group name ... defaulting to \"nogroup\"\n");
+
+	return ret;
+}
+
+static inline uid_t lookup_uid ( CHR name )
+{
+	struct passwd * pw = getpwnam(name);
+	uid_t uid = -1;
+
+	if (pw)
+		uid = pw->pw_uid;
+	else
+		warn("could not determine current user id ... defaulting to \"nobody\"\n");
+
+	return uid;
+}
+
+static inline gid_t lookup_gid ( CHR name )
+{
+	struct group * gr = getgrnam(name);
+	gid_t gid = -1;
+
+	if (gr)
+		gid = gr->gr_gid;
+	else
+		warn("could not determine current group id ... defaulting to \"nogroup\"\n");
+
+	return gid;
+}
+
+static inline char * lookup_pwd ( CHR name )
+{
+	struct passwd * pw = getpwnam(name);
+	char * pwd = "/tmp";
+
+	if (pw)
+		pwd = pw->pw_dir;
+	else
+		warn("could not determine current home ... defaulting to \"/tmp\"\n");
+
+	return pwd;
+}
+
+static inline char * lookup_shell ( CHR name )
+{
+	struct passwd * pw = getpwnam(name);
+	char * shell = "/bin/false";
+
+	if (pw)
+		shell = pw->pw_shell;
+	else
+		warn("could not determine current shell ... defaulting to \"/bin/false\"\n");
+
+	return shell;
+}
+
+static int xputenv(char * const name, char * const value)
+{
+	char *buf = (char*) malloc(strlen(name) + 1 + strlen(value) + 1);
+
+	if (!buf)
+		error("malloc() failed: %s\n", strerror(errno));
+
+	strcpy(buf, name);
+	strcat(buf, "=");
+	strcat(buf, value);
+
+	return putenv(buf);
+}
+
+static void do_su (entry_t * entry)
+{
+	uid_t new_uid;
+	gid_t new_gid;
+	char * new_pwd;
+	char * new_shell;
+
+	if ((new_uid = lookup_uid(entry->run_user)) < 0)
+		error("no such user \"%s\"\n", entry->run_user);
+
+	if ((new_gid = lookup_gid(entry->run_group)) < 0)
+		error("no such group \"%s\"\n", entry->run_group);
+
+	if ((new_pwd = lookup_pwd(entry->run_user)) < 0)
+		error("no such user \"%s\"\n", entry->run_user);
+
+	if ((new_shell = lookup_shell(entry->run_user)) < 0)
+		error("no such user \"%s\"\n", entry->run_user);
+
+	if (verbose)
+		fprintf(stderr, "Calling command : cmdline=\"%s\" user=\"%s\"(%d:%d) \n",
+			entry->run_command, entry->run_user, new_uid, new_gid );
+
+	/*
+	 * Environment
+	 */
+	if (xputenv("USER", entry->run_user))
+		error("putenv() failed: %s\n", strerror(errno));
+	if (xputenv("LOGNAME", entry->run_user))
+		error("putenv() failed: %s\n", strerror(errno));
+	if (xputenv("PWD", new_pwd))
+		error("putenv() failed: %s\n", strerror(errno));
+	if (xputenv("GROUP",   entry->run_group))
+		error("putenv() failed: %s\n", strerror(errno));
+	if (xputenv("HOME", new_pwd))
+		error("putenv() failed: %s\n", strerror(errno));
+	if (xputenv("SHELL", new_shell))
+		error("putenv() failed: %s\n", strerror(errno));
+
+	/*
+	 * Get real root back
+	 */
+	if (seteuid(euid) < 0) error("seteuid() failed: %s\n", strerror(errno));
+	if (setegid(egid) < 0) error("setegid() failed: %s\n", strerror(errno));
+
+	if (setregid(new_gid, new_gid))
+		error("setregid() failed: %s\n", strerror(errno));
+
+	if (initgroups(entry->run_user, new_gid))
+		error("initgroups() failed: %s\n", strerror(errno));
+
+	if (setreuid(new_uid, new_uid))
+		error("setreuid() failed: %s\n", strerror(errno));
+
+	if (chdir(new_pwd))
+		error("chdir() failed: %s\n", strerror(errno));
+
+	execve(entry->run_command, entry->params, environ);
+	error("execve() failed: %s\n", strerror(errno));
+}
+
+static char line[LINE_MAX];
+static entry_t* check_against_table (CHR filename, char *argv[])
+{
+	FILE *table;
+	char  *l = line;
+	char *cslot;
+	int   nslot, argc = 1, found = 0;
+	char *user  = get_current_user();
+	char *group = get_current_group();
+	entry_t *e = &entry;
+
+	if (!(table = fopen (filename, "r")))
+		error("Could not open %s: %s\n", filename, strerror(errno));
+
+	while (fgets(line, LINE_MAX, table)) {
+
+		if (*(l = (char*)line) == '#') continue;
+		if (*l == '\n') continue;
+
+		memset((void*)e, 0, sizeof(e));
+		nslot = 0;
+		argc = 1;
+		printf("====\n");
+		while ((cslot = strsep(&l, " \t\n")) && *cslot) {
+			switch (++nslot) {
+			case 1:
+				e->called_user = cslot;
+				printf("called_user=%s\n", cslot);
+				break;
+			case 2:
+				e->called_group = cslot;
+				printf("called_group=%s\n", cslot);
+				break;
+			case 3:
+				printf("called_command=%s\n", cslot);
+				e->called_command = cslot;
+				break;
+			case 4:
+				printf("called_cmdline=%s\n", cslot);
+				e->called_cmdline = cslot;
+				break;
+			case 5:
+				printf("run_user=%s\n", cslot);
+				e->run_user = cslot;
+				break;
+			case 6:
+				printf("run_group=%s\n", cslot);
+				e->run_group = cslot;
+				break;
+			case 7:
+				printf("run_cslot=%s\n", cslot);
+				e->run_command = cslot;
+				break;
+			default:
+				printf("run_params=%s\n", cslot);
+				e->params[argc++] = cslot;
+				break;
+			}
 		}
-	}
-	endpwent();
-	printf("could not determine current user name ... defaulting to nobody\n");
-	return "nobody";
-}
-
-char * get_current_group ()
-{
-	return "users";
-}
-
-int lookup_uid ( char * name )
-{
-	struct passwd * pw;
-	
-	setpwent();
-	
-	while (pw = getpwent()) {
-		if (strcmp(pw->pw_name, name)==0) return pw->pw_uid;
-	}
-	
-	endpwent();
-	return -1;
-}
-
-int lookup_gid ( char * name )
-{
-	/* FIXME!!!! */
-	
-	if (strcmp(name,"root")==0) return 0;
-	if (strcmp(name,"users")==0) return 100;
-	if (strcmp(name,"daemon")==0) return 2;
-	return -1;
-}
-
-int do_su ( CHR cmd, CHR cmdline )
-{
-	ENTRY * entry;
-	int new_uid;
-	int new_gid;
-	
-	entry = FindMatching ( get_current_user(), get_current_group(),
-		cmd, cmdline );
-	if (entry==NULL) {
-		printf("su_wrapper: permission denied\n");
-		return 0;
-	}
-
-	if (strcmp(entry->run_command, "-")==0) {
-		printf("su_wrapper: explicitly forbidden\n");
-		return 0;
-	}
-	new_uid = lookup_uid(entry->run_user);
-	if (new_uid==-1) {
-		printf("su_wrapper: no such user \"%s\"\n", entry->run_user);
-		return 0;
-	}
-	new_gid = lookup_gid(entry->run_group);
-
-	if (verbose) {
-	    printf("Calling command : cmdline=\"%s\" user=\"%s\"(%d:%d) \n", 
-		entry->run_command, entry->run_user, new_uid, new_gid );
-	}
-	
-	if (setregid(new_gid, new_gid)==-1) {
-//		printf("su_wrapper: setgid() permission denied ! \n");
-//		return 0;
-	}
-
-	if (setreuid(new_uid, new_uid)==-1) {
-		printf("su_wrapper: setreuid() permission denied ! \n");
-		printf("            did you set the SUID bit on the su_wrapper executable ?\n");
-		return 0;
-	}
-	
-	return system ( entry->run_command );
-}
-
-int read_line ( int fd, char * buffer )
-{
-	int ret;
-	char * buf;
-	
-	buf = buffer;
-	
-	while (1) {
-__loop:
-	    ret = read(fd, buf, 1);
-	    if (ret==0) goto __eof;
-	    if (ret!=1) goto __err;
-	    
-	    if ((*buf)==10) goto __ln;
-	    if ((*buf)==13) goto __ln;
-	    buf++;
-	}
-__err:
-	*buf = 0;
-	return -errno;
-__eof:	
-	*buf = 0;
-	if (buf==buffer) return 0;
-	return 1;
-__ln:
-	if (buf==buffer) goto __loop;
-	*buf = 0;	    
-	return 1;
-}
-
-int load_table ( CHR filename )
-{
-	int fd;
-	char buffer[512];
-	int ret;
-	char * chr;
-	int argc;
-	char * argv[16];
-	int x;
-
-	fd = open ( filename, O_RDONLY );
-	if (fd<0) {
-		printf("WARN: su_wrapper: could not load table \"%s\"\n", filename);
-		return 0;
-	}
-	
-	while (ret=read_line(fd, (char*)&buffer)==1) {
-		/* skip out comments */
-		chr = (char*)&buffer;
-		while (*chr) {
-			if ((*chr)=='#') *chr = 0;
-			else chr++;
-		}
-		if (strcmp((char*)&buffer,"")==0);
-		else {
-			char * __user      = "";
-			char * __group     = "";
-			char * __command   = "";
-			char * __cmdline   = "";
-			char * __param     = "";
-			char * __r_user    = "";
-			char * __r_group   = "";
-			char * __r_cmdline = "";
+		if (!wildcard_match(e->called_command, my_basename(argv[0])))
+			continue;
+		if (!wildcard_match(e->called_user,    user))
+			continue;
+		if (!wildcard_match(e->called_group,   group))
+			continue;
+//		if (!cmdline_match(e->called_cmdline,  (argv+1)))
+//			continue;
+		if (!cmdline_match(e->called_cmdline, argv))
+			continue;
 		
-			argc = SplitCommandLine ( (char*)&buffer, (char**)&argv );
-			
-			if (argc>0) __user      = argv[0];
-			if (argc>1) __group     = argv[1];
-			if (argc>2) __command   = argv[2];
-			if (argc>3) __cmdline   = argv[3];
-			if (argc>4) __r_user    = argv[4];
-			if (argc>5) __r_group   = argv[5];
-			if (argc>6) __r_cmdline = argv[6];
-			
-			add_entry ( __user, __group, __command, __cmdline,
-			    __r_user, __r_group, __r_cmdline, __param );
-		}
+		if (e->run_command && (!strcmp(e->run_command, "-")))
+			error("explicitly forbidden\n");
+
+		found++;
+		break;
 	}
-	
-	close ( fd );
-	return 1;
+
+	if (!found)
+		e = NULL;
+	else {
+		e->params[0] = e->called_command; /* first argument */
+		if (*(argv+1)) {
+			char ** ptr = (argv+1);
+			int n;
+
+			for (n = 0; n < _POSIX_ARG_MAX && ptr[n] ; n++)
+				e->params[argc++] = ptr[n];
+		}
+		e->params[argc] = NULL;		  /* last argument */
+	}
+
+	fclose(table);
+	return e;
 }
 
 int main (int argc, char * argv[])
 {
-	char cmd_buffer[1024];
-	int x;
-	
-	cmd_buffer[0] = 0;
-	x = 1;
+	entry_t *entry = NULL;
+	char * term;
 
-	if ((argc>1)&&(strcmp(argv[1],"--verbose")==0)) {
-	    verbose = 1;
-	    x++;
+	egid = getegid(); rgid = getgid();
+	if (setegid(rgid) < 0) error("setegid() failed: %s\n", strerror(errno));
+	euid = geteuid(); ruid = getuid();
+	if (seteuid(ruid) < 0) error("seteuid() failed: %s\n", strerror(errno));
+
+	
+	if (!strcmp(myname, my_basename(argv[0]))) {
+		argc--;
+		argv++;
 	}
+
+	entry = check_against_table(CONFIGFILE, argv);
+	if (!entry)
+		error("permission denied\n");
+
+	if (getenv("SUWRAP_DEBUG"))
+		verbose++;
+
+	term = getenv("TERM");
+
+	verbose++;
 	
-	while (x<argc) {
-	    strcat ( (char*)&cmd_buffer, argv[x] );
-	    if (x<argc) strcat ( (char*)&cmd_buffer, " " );
-	    x++;
-	}
-	
-	load_table( CONFIGFILE );
-	
-	do_su ( extract(argv[0]), (char*)&cmd_buffer );
+
+#if 0
+	/*
+	 * Clear sensitive environment variables.
+	 * Overwriting of personal environment variables will be done by do_su.
+	 */
+	unsetenv("LD_LIBRARY_PATH");	/* runtime linker */
+	unsetenv("LD_PRELOAD");
+	unsetenv("LD_DEBUG");
+	unsetenv("LD_DEBUG_OUTPUT");
+	unsetenv("LD_AOUT_LIBRARY_PATH");
+	unsetenv("LD_AOUT_PRELOAD");
+	unsetenv("BASH_ENV");		/* bash source file */
+	unsetenv("ENV");		/* sh source file */
+	unsetenv("DEFAULT_ENV");	/* ksh source file */
+	unsetenv("SHINIT");		/* ash source file */
+	unsetenv("MAIL");		/* mail default folder */
+	unsetenv("MALLOC_CHECK_");	/* malloc(3) */
+	/* something forgotten? */
+#else
+	/*
+	 * Overwrite environment with a new and valid pointer of strings.
+	 * Overwriting of personal environment variables will be done by do_su.
+	 */
+	environ = (char **) malloc (2 * sizeof (char *));
+	if (!environ)
+		error("malloc() failed: %s\n", strerror(errno));
+	memset(environ, 0, 2 * sizeof (char *));
+#endif
+
+	/*
+	 * overwrite PATH environment variable.
+	 */
+	if (!strcmp(entry->run_user, "root"))
+		xputenv("PATH", "/sbin:/usr/sbin:/bin:/usr/bin:/usr/bin/X11");
+	else
+		xputenv("PATH", "/usr/local/bin:/bin:/usr/bin:/usr/bin/X11");
+	if (errno)
+		error("putenv() failed: %s\n", strerror(errno));
+	if (xputenv("TERM", term))
+		error("putenv() failed: %s\n", strerror(errno));
+
+	do_su (entry);
+
+	return 0; /* not reached */
 }
